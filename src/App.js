c12273b4523f3d6d87e683f3097139203b1a5378
Steve Astels <sastels@gmail.com>
2019-10-02 13:52:04 -0400

    hard code yml

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Box, Button, Heading, Grommet } from "grommet";
 import { Next } from "grommet-icons";
+import yaml from "js-yaml";
 
 const theme = {
   global: {
@@ -26,8 +27,51 @@ const AppBar = props => (
   />
 );
 
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array#2450976
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
 const App = () => {
   const [showSidebar, setShowSidebar] = useState(false);
+  const [teamData, setTeamData] = useState(undefined);
+  const [teamDataIndex, setTeamDataIndex] = useState(0);
+
+  useEffect(() => {
+    try {
+      fetch("team.yml")
+        .then(raw => {
+          return raw.text();
+        })
+        .then(text => {
+          return yaml.safeLoad(text);
+        })
+        .then(data => {
+          console.log(data);
+          setTeamData(shuffle(data.exec.concat(data.team)));
+        });
+    } catch (e) {
+      console.log(e);
+    }
+  }, []);
+
+  const person = teamData ? teamData[teamDataIndex] : undefined;
 
   return (
     <Grommet theme={theme} full>
@@ -37,12 +81,28 @@ const App = () => {
             CDS Team
           </Heading>
           <Button label="Reveal" onClick={() => setShowSidebar(!showSidebar)} />
-          <Button label="Next" icon={<Next />} reverse onClick={() => {}} />
+          <Button
+            label="Next"
+            icon={<Next />}
+            reverse
+            onClick={() => {
+              if (teamData && teamDataIndex < teamData.length - 1) {
+                setTeamDataIndex(teamDataIndex + 1);
+              } else {
+                setTeamDataIndex(0);
+              }
+            }}
+          />
         </AppBar>
 
         <Box direction="column" flex overflow={{ horizontal: "hidden" }}>
           <Box flex align="center" justify="center">
-            app body
+            {person && (
+              <img
+                src={`https://digital.canada.ca${person["image-name"]}`}
+                alt={person.name}
+              />
+            )}
           </Box>
           <Box
             height="20%"
@@ -51,7 +111,12 @@ const App = () => {
             align="center"
             justify="center"
           >
-            {showSidebar && <div>Steve</div>}
+            {showSidebar && person && (
+              <React.Fragment>
+                <div>{person.name}</div>
+                <div>{person.title.en}</div>
+              </React.Fragment>
+            )}
           </Box>
         </Box>
       </Box>
